21cf0db93ec528b9e0056d22657ff4f40053663e
miiilyy <emili.silva@estudante.ifms.edu.br>
2023-09-14 10:10:00 -0400

    Novo

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -35,154 +35,681 @@
 
 //FIND ({QUERY},{PROJEÇÃO})
 
-use("teretreino");
-db.createCollection("treino");
+//use("teretreino");
+//db.createCollection("treino");
 
-db.treino.insertOne(
-    {
-        nome:"Peito e Biceps",
-        diaSemana:"Segunda",
-        exercicios:[
-            {
-                nome:"Supino",
-                serie:4,
-                repeticoes:15
-            },
-            {
-                nome:"Rosca Martelo",
-                serie:4,
-                repeticoes:15
-            },
-            {
-                nome:"Voador",
-                serie:4,
-                repeticoes:15
-            },
-            {
-                nome:"Rosca Scott",
-                serie:4,
-                repeticoes:15
-            },
+//db.treino.insertOne(
+  //  {
+  //      nome:"Peito e Biceps",
+    //      diaSemana:"Segunda",
+    //      exercicios:[
+      //        {
+      //            nome:"Supino",
+           //       serie:4,
+       //           repeticoes:15
+            //  },
+    //          {
+        //          nome:"Rosca Martelo",
+            //      serie:4,
+       //           repeticoes:15
+           //   },
+            //  {
+                //  nome:"Voador",
+   //               serie:4,
+       //           repeticoes:15
+           //   },
+           //   {
+              //    nome:"Rosca Scott",
+              //    serie:4,
+       //           repeticoes:15
+          //    },
             
-        ]
-    }
-)
-db.treino.insertOne(
-    {
-        nome:"Costas e Tríceps",
-        diaSemana:"sexta",
-        exercicios:[
-            {
-                nome:"Tríceps Polia",
-                serie:4,
-                repeticoes:15
-            },
-            {
-                nome:"Tríceps Corda",
-                serie:4,
-                repeticoes:15
-            },
-            {
-                nome:"Pull Down",
-                serie:4,
-                repeticoes:15
-            },
-            {
-                nome:"Remada Curvada",
-                serie:4,
-                repeticoes:15
-            },
-            {
-                nome:"Puxada Alta",
-                serie:4,
-                repeticoes:15
-            },
-            {
-                nome:"Remada Baixa",
-                serie:4,
-                repeticoes:15
-            }
-        ]
-    }
-)
+         // ]
+    //  }
+ // )
+ // db.treino.insertOne(
+    //  {
+        //  nome:"Costas e Tríceps",
+        //  diaSemana:"sexta",
+         // exercicios:[
+            //  {
+                //  nome:"Tríceps Polia",
+               //   serie:4,
+              //    repeticoes:15
+            //  },
+           //   {
+               //   nome:"Tríceps Corda",
+              //    serie:4,
+                //  repeticoes:15
+            //  },
+            //  {
+                //  nome:"Pull Down",
+               //   serie:4,
+               //   repeticoes:15
+           //   },
+            //  {
+                //  nome:"Remada Curvada",
+                //  serie:4,
+              //    repeticoes:15
+           //   },
+           //   {
+               //   nome:"Puxada Alta",
+                //  serie:4,
+                //  repeticoes:15
+           //   },
+            //  {
+               //   nome:"Remada Baixa",
+                //  serie:4,
+                //  repeticoes:15
+            //  }
+        //  ]
+    //  }
+ // )
 
-db.treino.insertOne(
-    {
-        nome:"Inferiores",
-        diaSemana:"Terca e Quinta",
-        exercicios:[
-            {
-                nome:"Leg Press",
-                serie:4,
-                repeticoes:12
-            },
-            {
-                nome:"Agachamento",
-                serie:4,
-                repeticoes:12
-            },
-            {
-                nome:"Levantamento Terra",
-                serie:4,
-                repeticoes:12
-            },
-            {
-                nome:"Panturrilha",
-                serie:4,
-                repeticoes:15
-            },
-            {
-                nome:"Cadeira Extensora ",
-                serie:4,
-                repeticoes:15
-            },
-            {
-                nome:"Cadeira Abdutora",
-                serie:4,
-                repeticoes:15
-            },
-            {
-                nome:"Cadeira Adutora",
-                serie:4,
-                repeticoes:15
-            }
+// db.treino.insertOne(
+//     {
+//         nome:"Inferiores",
+//         diaSemana:"Terca e Quinta",
+//         exercicios:[
+//             {
+//                 nome:"Leg Press",
+//                 serie:4,
+//                 repeticoes:12
+//             },
+//             {
+//                 nome:"Agachamento",
+//                 serie:4,
+//                 repeticoes:12
+//             },
+//             {
+//                 nome:"Levantamento Terra",
+//                 serie:4,
+//                 repeticoes:12
+//             },
+//             {
+//                 nome:"Panturrilha",
+//                 serie:4,
+//                 repeticoes:15
+//             },
+//             {
+//                 nome:"Cadeira Extensora ",
+//                 serie:4,
+//                 repeticoes:15
+//             },
+//             {
+//                 nome:"Cadeira Abdutora",
+//                 serie:4,
+//                 repeticoes:15
+//             },
+//             {
+//                 nome:"Cadeira Adutora",
+//                 serie:4,
+//                 repeticoes:15
+//             }
             
-        ]
-    }
-)
-use ("teretreino");
-db.treino.find({_id:ObjectId("64f7421cfd8efefbf88f565d")})
+//         ]
+//     }
+// )
+// use ("teretreino");
+// db.treino.find({_id:ObjectId("64f7421cfd8efefbf88f565d")})
 
-use ("teretreino");
-db.treino.updateOne(
-    {_id:ObjectId("64f7421cfd8efefbf88f565f")},
-    {
-        $set:{
-            diaSemana:"sexta",
-        nome:"peito e biceps A"
-        }
-    }
-)
+// use ("teretreino");
+// db.treino.updateOne(
+//     {_id:ObjectId("64f7421cfd8efefbf88f565f")},
+//     {
+//         $set:{
+//             diaSemana:"sexta",
+//         nome:"peito e biceps A"
+//         }
+//     }
+// )
 
-use("teretreino");
-db.treino.updateOne(
-    {_id:ObjectId("64f7421cfd8efefbf88f565f"), "exercicio.nome":"Voador"},
-    {
-        $set:{
-            "exercicios.$.nome":"Perck Deck"
-        }
-    }
+// use("teretreino");
+// db.treino.updateOne(
+//     {_id:ObjectId("64f7421cfd8efefbf88f565f"), "exercicio.nome":"Voador"},
+//     {
+//         $set:{
+//             "exercicios.$.nome":"Perck Deck"
+//         }
+//     }
+// )
+// use("teretreino");
+// db.treino.updateOne(
+//     {_id:ObjectId("64f7421cfd8efefbf88f565d"), "exercicios.nome":"Rosca Scott"},
+//     {
+//         $set:{
+//             "exercicios.$.nome":"Rosca Scott",
+//             "exercicios.$.serie":5,
+//             "exercicios.$.repeticoes":12
+//         }
+//     }
+// )
+
+db.createCollection("cake");
+
+use("db_patissier");
+db.cake.insertMany([
+{
+    _id:ObjectId ("111111111111"), 
+    nome: "Bolo de Chocolate", 
+    peso: "1,350Kg", 
+    preco:99.70, 
+    ingredientes:[
+       {
+        nome: "ovo",
+        quantidade: 4
+       },
+       {
+        nome: "acucar",
+        quantidade: "2 xicaras"
+       },
+       {
+        nome: "leite",
+        quantidade: "200ml"
+       },
+       {
+        nome: "chocolate",
+        quantidade: "1 xicara"
+       },
+       {
+        nome: "farinha de trigo",
+        quantidade: "2 xicaras"
+       },
+       {
+        nome: "fermento",
+        quantidade: "1 colher de sopa"
+       }
+    ] 
+},
+{
+    _id:ObjectId ("222222222222"), 
+    nome: "Bolo de Cenoura", 
+    peso: "1.950Kg", 
+    preco: 70.00, 
+    ingredientes:[
+       {
+        nome: "ovo",
+        quantidade: 4
+       },
+       {
+        nome: "acucar",
+        quantidade: "2 xicaras"
+       },
+       {
+        nome: "leite",
+        quantidade: "200ml"
+       },
+       {
+        nome: "cenoura",
+        quantidade: "2 cenouras medias"
+       },
+       {
+        nome: "farinha de trigo",
+        quantidade: "2 xicaras"
+       },
+       {
+        nome: "fermento",
+        quantidade: "1 colher de sopa"
+       }
+    ] 
+},
+{
+    _id:ObjectId ("333333333333"), 
+    nome: "Bolo de Limão", 
+    peso: "950g", 
+    preco:60.00, 
+    ingredientes:[
+       {
+        nome: "ovo",
+        quantidade: 4
+       },
+       {
+        nome: "acucar",
+        quantidade: "2 xicaras"
+       },
+       {
+        nome: "leite",
+        quantidade: "200ml"
+       },
+       {
+        nome: "limão",
+        quantidade: "1/2 xicara de suco de limão"
+       },
+       {
+        nome: "farinha de trigo",
+        quantidade: "2 xicaras"
+       },
+       {
+        nome: "fermento",
+        quantidade: "1 colher de sopa"
+       }
+    ] 
+},
+{
+    _id:ObjectId ("444444444444"), 
+    nome: "Bolo de Trigo", 
+    peso: "500g", 
+    preco:40.00, 
+    ingredientes:[
+       {
+        nome: "ovo",
+        quantidade: 4
+       },
+       {
+        nome: "acucar",
+        quantidade: "2 xicaras"
+       },
+       {
+        nome: "leite",
+        quantidade: "200ml"
+       },
+       {
+        nome: "farinha de trigo",
+        quantidade: "2 xicaras"
+       },
+       {
+        nome: "fermento",
+        quantidade: "1 colher de sopa"
+       }
+    ] 
+}]
 )
-use("teretreino");
-db.treino.updateOne(
-    {_id:ObjectId("64f7421cfd8efefbf88f565d"), "exercicios.nome":"Rosca Scott"},
-    {
-        $set:{
-            "exercicios.$.nome":"Rosca Scott",
-            "exercicios.$.serie":5,
-            "exercicios.$.repeticoes":12
-        }
-    }
-)
+use("db_patissier")
+db.cake.deleteMany({_id:ObjectId("65008332649218fb41095b8a")});
+
+
+
+
+            
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -1,52 +1,136 @@
-import { useState } from "react";
-import Filme from "../filme/Filme";
+import { useState } from 'react'
+import Filme from './../filme/Filme'
 import './Main.css'
-type FilmesType ={
-    titulo:string
-    sinpse:string
+type FilmeType = {
+    id:number,
+    titulo:string,
+    sinopse:string,
     imagem:string
 }
-export default function Main(){
-    //Hook
-    const [texto,setTexto] = useState("Guilherme")
-        function mudaTexto(e:React.ChangeEvent<HTMLInputElement>){
-            console.log(e.target.value)
-            setTexto ("Tere")
-        }
-    return(
-        <>
-        <div className="pesquisa">
-            <p>Buscar Filme</p>
-    <input className='barrapesquisa' type='text' onChange={mudaTexto}/>
-    <div>
-    <p className='texto_digitado'>pesquisa:{texto}</p>
-    </div>
-    </div>
-    <main className="content-main"> 
-            <Filme titulo="Barbie" sinopse="Depois de ser expulsa da 
-                    Barbieland por ser uma boneca 
-                    de aparência menos do que 
-                    perfeita, Barbie parte para o 
-                    mundo humano em busca da 
-                    verdadeira felicidade."
-                    imagem="/barbie.png"
-                    />
-            <Filme titulo="Transformers" sinopse="O destino da humanidade está em jogo quando duas raças de robôs, os Autobots e os vilões Decepticons,
-             chegam à Terra. Os robôs possuem a capacidade de se transformarem em diferentes objetos mecânicos enquanto buscam a chave do poder supremo com a ajuda do jovem Sam."
-                    imagem="/250px-Transformers-poster.jpg"
-                    />
-                     <Filme titulo="Transformers" sinopse="O destino da humanidade está em jogo quando duas raças de robôs, os Autobots e os vilões Decepticons,
-             chegam à Terra. Os robôs possuem a capacidade de se transformarem em diferentes objetos mecânicos enquanto buscam a chave do poder supremo com a ajuda do jovem Sam."
-                    imagem="/250px-Transformers-poster.jpg"
-                    />
-                     <Filme titulo="Transformers" sinopse="O destino da humanidade está em jogo quando duas raças de robôs, os Autobots e os vilões Decepticons,
-             chegam à Terra. Os robôs possuem a capacidade de se transformarem em diferentes objetos mecânicos enquanto buscam a chave do poder supremo com a ajuda do jovem Sam."
-                    imagem="/250px-Transformers-poster.jpg"
-                    />
 
-        </main>
-    </>
+export default function Main() {
+    //let textodigitado = 'Barbie'
+    //Hooks são funções do React que ajudam a gente a fazer tarefas
+    //específicas
+    const [texto,setTexto]=useState("")
+
+    const filmes:FilmeType[] = [
+        {
+            id:1,
+            titulo:'Barbie',
+            sinopse:"Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
+            imagem:'/barbie.png'
+        },
+        {
+            id:2,
+            titulo:'Filme Transformers',
+            sinopse:'O destino da humanidade está em jogo quando duas raças de robôs, os Autobots e os vilões Decepticons, chegam à Terra. Os robôs possuem a capacidade de se transformarem em diferentes objetos mecânicos enquanto buscam a chave do poder supremo com a ajuda do jovem Sam.',
+            imagem:'/250px-Transformers-poster.jpg'
+        },
+        {
+            id:3,
+            titulo:'Filme Transformers',
+            sinopse:'O destino da humanidade está em jogo quando duas raças de robôs, os Autobots e os vilões Decepticons, chegam à Terra. Os robôs possuem a capacidade de se transformarem em diferentes objetos mecânicos enquanto buscam a chave do poder supremo com a ajuda do jovem Sam.',
+            imagem:'/250px-Transformers-poster.jpg'
+        },
+        {
+            id:5,
+            titulo:'Barbie',
+            sinopse:"Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
+            imagem:'/barbie.png'
+        },
+    ]
+
+    //O parâmetro "e" da minha função será o meu evento que ocorreu
+    function TrataTexto(e:React.ChangeEvent<HTMLInputElement>){
+        //console.log(e.target.value)
+        //Como eu faço para mudar o texto para "TERE"
+        setTexto(e.target.value)
+    }
+    return (
+        <>
+            <div className="campo_pesquisa">
+                <p>Busque um filme</p>
+                <input type="text" 
+                       className='botao_pesquisa'
+                       placeholder='Pesquise um Filme'
+                       onChange={TrataTexto} />
+                {texto && <p>Resultados Para: {texto} </p>}
+            </div>
+            <main className="content-main">
+                {/* 
+                    Use algo do vetor para tentar criar os filmes 
+                */}
+                {
+                    filmes.filter((filme)=>filme.titulo.toLowerCase().includes(texto)).map(
+                        (filme)=>
+                            <Filme 
+                                key={filme.id}
+                                sinopse={filme.sinopse}
+                                titulo={filme.titulo}
+                                imagem={filme.imagem}
+                            />
+                    )
+                }
+
+                
+                {/* <Filme titulo='Barbie'
+                    sinopse='Depois de ser expulsa da 
+                   Barbieland por ser uma boneca de aparência 
+                   menos do que perfeita, Barbie parte para o 
+                   mundo humano em busca da verdadeira felicidade.'
+                    imagem='/barbie.png'
+                />
+                <Filme titulo='Filme Barbie'
+                    sinopse='Depois de ser expulsa da 
+                   Barbieland por ser.'
+                    imagem='/KEN.png'
+                />
+                <Filme titulo='Barbie'
+                    sinopse='Depois de ser expulsa da 
+                   Barbieland por ser uma boneca de aparência 
+                   menos do que perfeita, Barbie parte para o 
+                   mundo humano em busca da verdadeira felicidade.'
+                    imagem='/barbie.png'
+                />
+                <Filme titulo='Filme Barbie'
+                    sinopse='Depois de ser expulsa da 
+                   Barbieland por ser uma boneca de aparência 
+                   menos do que perfeita, Barbie parte para o 
+                   mundo humano em busca da verdadeira felicidade.'
+                    imagem='/boneca.jpg'
+                />
+                <Filme titulo='Barbie'
+                    sinopse='Depois de ser expulsa da 
+                   Barbieland por ser uma boneca de aparência 
+                   menos do que perfeita, Barbie parte para o 
+                   mundo humano em busca da verdadeira felicidade.'
+                    imagem='/barbie.png'
+                />
+                <Filme titulo='Filme Barbie'
+                    sinopse='Depois de ser expulsa da 
+                   Barbieland por ser uma boneca de aparência 
+                   menos do que perfeita, Barbie parte para o 
+                   mundo humano em busca da verdadeira felicidade.'
+                    imagem='/boneca.jpg'
+                />
+                <Filme titulo='Barbie'
+                    sinopse='Depois de ser expulsa da 
+                   Barbieland por ser uma boneca de aparência 
+                   menos do que perfeita, Barbie parte para o 
+                   mundo humano em busca da verdadeira felicidade.'
+                    imagem='/barbie.png'
+                />
+                <Filme titulo='Filme Barbie'
+                    sinopse='Depois de ser expulsa da 
+                   Barbieland por ser uma boneca de aparência 
+                   menos do que perfeita, Barbie parte para o 
+                   mundo humano em busca da verdadeira felicidade.'
+                    imagem='/boneca.jpg'
+                /> */}
+
+            </main>
+        </>
     )
 }
-
 
